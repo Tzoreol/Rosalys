a3503526df4bb284612ef792004247d57f32bcfe
Kévin Pausé <tzoreol@gmail.com>
2016-03-15 19:57:24 +0100

    slideshow

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -7,11 +7,11 @@ function doShow() {
     var showWidth = $('#show').width();
     var nextWidth = next.width();
     active.fadeOut(2000);
-    next.fadeIn(2000).offset({left: $('#show').offset().left +(showWidth-nextWidth)/2});
+    next.fadeIn(2000).offset({left: $('#show').offset().left +(showWidth-nextWidth)/2, top: $('#show').offset().top});
 }
 $(document).ready(function(){
     var showWidth = $('#show').width();
     var imgWidth = $('#show img').width();
-    $('#show img').offset({left: $('#show').offset().left +(showWidth-imgWidth)/2});
+    $('#show img').offset({left: $('#show').offset().left +(showWidth-imgWidth)/2, top: $('#show').offset().top});
     setInterval(doShow, 5000);
 });
